7080b15aec94c41b9d0ad958d3f3382c472d4189
Monika Kairaityte <monika@kibit.lt>
2026-01-16 20:44:02 +0200

    Add script for creating podman-compose-5.4.2 .deb files
    Signed-off-by: Monika Kairaityte <monika@kibit.lt>

---
+++ scripts/create_podman_5.4.2_deb_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update -y
+DEBIAN_FRONTEND=noninteractive apt-get install -y git
+git clone --depth 1 --branch debian-5.4.2 https://github.com/p12tic/podman-compose-test-podman.git podman-5.4.2
+cd podman-5.4.2
+echo 'deb http://deb.debian.org/debian bookworm-backports main' >/etc/apt/sources.list.d/backports.list
+apt-get update -y
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends dpkg-dev devscripts libgpgme-dev libseccomp-dev build-essential libsystemd-dev bash-completion conmon dh-golang go-md2man golang-dbus-dev golang-go libapparmor-dev libbtrfs-dev libdevmapper-dev libglib2.0-dev libsubid-dev
+DEBIAN_FRONTEND=noninteractive apt-get install -y golang-1.22/bookworm-backports
+rm /usr/bin/go
+ln -s /usr/lib/go-1.22/bin/go /usr/bin/go
+DEBIAN_FRONTEND=noninteractive apt remove -y golang-1.19
+go mod tidy
+git archive --worktree-attributes --prefix=podman-5.4.2+composetest/ HEAD --format=tar.gz -o ../podman_5.4.2+composetest.orig.tar.gz
+# Geras:
+go install -trimpath -v -p 8 -tags "apparmor seccomp selinux systemd libsubid" -ldflags "-X github.com/containers/podman/libpod/define.buildInfo=1753462038 -X github.com/containers/podman/libpod/define.buildOrigin=Debian" ./cmd/podman
+DEB_BUILD_OPTIONS=nocheck debuild -us -uc
